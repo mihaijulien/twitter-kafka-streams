6b85061c6361d5efe188f7529f0efa97757c2b24
Julien Mihai <mihaijulien@gmail.com>
2022-01-17 20:50:48 +0200

    Add config variables in application.yml and add initialization messages using CommandLineRunner

---
+++ twitter-to-kafka-service/src/main/java/io/github/mihaijulien/twitter/kafka/service/TwitterToKafkaServiceApplication.java
@@ -0,0 +1,30 @@
+package io.github.mihaijulien.twitter.kafka.service;
+
+import io.github.mihaijulien.twitter.kafka.service.config.TwitterToKafkaServiceConfigData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class TwitterToKafkaServiceApplication implements CommandLineRunner {
+
+    private static final Logger LOG = LoggerFactory.getLogger(TwitterToKafkaServiceApplication.class);
+    private final TwitterToKafkaServiceConfigData twitterToKafkaServiceConfigData;
+
+    public TwitterToKafkaServiceApplication(TwitterToKafkaServiceConfigData twitterToKafkaServiceConfigData) {
+        this.twitterToKafkaServiceConfigData = twitterToKafkaServiceConfigData;
+    }
+
+    public static void main(String[] args){
+        SpringApplication.run(TwitterToKafkaServiceApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        LOG.info(Arrays.toString(twitterToKafkaServiceConfigData.getTwitterKeywords().toArray(new String[] {})));
+    }
+}
