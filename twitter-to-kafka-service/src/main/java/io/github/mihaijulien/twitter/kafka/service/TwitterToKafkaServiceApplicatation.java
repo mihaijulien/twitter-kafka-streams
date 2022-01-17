21c282c076b77c1f4939110bdf3d76ec10cfa51a
Julien Mihai <mihaijulien@gmail.com>
2022-01-17 20:02:50 +0200

    Init twitter-to-kafka service

---
+++ twitter-to-kafka-service/src/main/java/io/github/mihaijulien/twitter/kafka/service/TwitterToKafkaServiceApplicatation.java
@@ -0,0 +1,12 @@
+package io.github.mihaijulien.twitter.kafka.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterToKafkaServiceApplicatation {
+
+    public static void main(String[] args){
+        SpringApplication.run(TwitterToKafkaServiceApplicatation.class, args);
+    }
+}
